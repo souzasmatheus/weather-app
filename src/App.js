1b0e9f2b083a07979a36b33864d62d66d73e4fed
Matheus Souza <matheu.sk.9@hotmail.com>
2019-07-27 14:54:07 -0300

    Create search elements and pass info into component's state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Acert</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    city: '',
+    state: '',
+    isLoading: false
+  };
+
+  renderStates() {
+    const states = [
+      'AC',
+      'AL',
+      'AP',
+      'AM',
+      'BA',
+      'CE',
+      'DF',
+      'ES',
+      'GO',
+      'MA',
+      'MT',
+      'MS',
+      'MG',
+      'PA',
+      'PB',
+      'PR',
+      'PE',
+      'PI',
+      'RJ',
+      'RN',
+      'RS',
+      'RO',
+      'RR',
+      'SC',
+      'SP',
+      'SE',
+      'TO'
+    ];
+
+    const statesEl = states.map(state => (
+      <option key={`state-${state}`} value={state}>
+        {state}
+      </option>
+    ));
+
+    statesEl.push(
+      <option disabled selected>
+        UF
+      </option>
+    );
+
+    return statesEl;
+  }
+
+  search() {
+    this.setState({ isLoading: true });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <input
+            onChange={e => this.setState({ city: e.target.value })}
+            type="text"
+            placeholder="Informe a cidade"
+          />
+          <select onChange={e => this.setState({ state: e.target.value })}>
+            {this.renderStates()}
+          </select>
+          <input type="submit" value="Buscar" onClick={() => this.search()} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
