a55f0377a01f354fa4c54fa4088f4215c4060282
Matheus Souza <matheu.sk.9@hotmail.com>
2019-07-29 16:27:25 -0300

    Updates

---
+++ precache-manifest.16291c27b43803526f10f772d7f47534.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367610243d4bd00ddfc7a8073f93de36",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "16caa58d43264001fb7b",
+    "url": "/weather-app/static/css/main.e5a1600b.chunk.css"
+  },
+  {
+    "revision": "229eab1971eecb65b586",
+    "url": "/weather-app/static/js/2.22c368b7.chunk.js"
+  },
+  {
+    "revision": "16caa58d43264001fb7b",
+    "url": "/weather-app/static/js/main.adcfaa62.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
